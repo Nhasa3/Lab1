5e0b603ace26918d61e4f1df53f515c5647625a1
NAH13 <nhasa0011@gmail.com>
2024-09-19 17:38:27 -0400

    Program has completed with desired output

--- student_grade.c
+++ student_grade.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
+/*  Name: Nesar ahmad Hasanzada
+    Student number: 041111577
+    Lab1
+    */
 
-
+/* Macros for the program defined*/
 #define MAX_STUDENTS 100
 #define PASSING_GRADE 60
 
+/* Main function*/
 int main(){
+    /*Declaring the variables*/
     int num_student, i;
-    int count = 0;
-    
+
 printf("Enter the number of students: ");
-if(scanf("%d" , &num_student) > MAX_STUDENTS){
-    printf("Maximum number of students exceeded");
+scanf("%d" , &num_student);
+/*checks if the num_students be higher than maximum volume*/
+if(num_student > MAX_STUDENTS){
+    printf("Maximum number of students exceeded\n");
+    return 1;  
 }
-   char student_name[num_student][10];
-    int student_grade[num_student];
-    char grade;
+/*Declaring variables*/
+char student_name[num_student][10];     /*character string of student name with size of 10 characters*/
+    int student_grade[num_student];         /*character string of student grade*/
+    char grade[num_student];                /*Array of grade*/
+
+/*Loop through asking student name and grade till num_student*/
 for(i=0;i<num_student;i++){
 
 printf("Enter student name:");
@@ -22,18 +33,25 @@ scanf("%s", student_name[i]);
 
 printf("Enter student grade:");
 scanf("%d", &student_grade[i]);
-count++;
+/*checks if the student_grade has a valid value*/
+if(student_grade[i] < 1 || student_grade[i] > 100){
+    printf("Invalid input, students grade are 1 to 100.\n");
+    return 1;
 }
 
-printf("The list of students.\n");
-printf("Student name    student_mark    grade\n");
-for(i=0;i<num_student;i++){
-    if(student_grade < PASSING_GRADE){
-    grade = 'F';
+/*checks if the student has met course standart*/
+    if(student_grade[i] < PASSING_GRADE){
+    grade[i] = 'F';
 }else{
-    grade = 'P';
+    grade[i] = 'P';
 }
-    printf("%s               %d                 %c", student_name[i] ,student_grade[i], grade);
+}
+
+/*List the students with their names and grades*/
+printf("The list of students.\n");
+printf("Student name    |  student_mark     |  grade\n");
+for(i=0;i<num_student;i++){
+    printf("%s               %d                 %c", student_name[i] ,student_grade[i], grade[i]);
     printf("\n");
 }
 
